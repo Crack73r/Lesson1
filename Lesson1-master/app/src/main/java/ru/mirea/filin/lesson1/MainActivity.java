ed0881e517e42e86178c8849b9deee10516dfec2
Crack73r <106272796+Crack73r@users.noreply.github.com>
2023-09-10 21:53:04 +0300

    Add files via upload

---
+++ Lesson1-master/app/src/main/java/ru/mirea/filin/lesson1/MainActivity.java
@@ -0,0 +1,20 @@
+package ru.mirea.filin.lesson1;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.CheckBox;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        TextView view = (TextView) findViewById(R.id.textView2);
+        view.setText("Lorem ipsum");
+        CheckBox checkBox = findViewById(R.id.checkBox2);
+        checkBox.setChecked(true);
+    }
+}
